5cfa623b3b8b741d23eea469dae2b85c679bcda0
Angelica-Aggio-Cruz-Prado-da-Silva <angelicaprado.1096@gmail.com>
2022-11-04 20:18:48 -0300

    iniciando o projeto

---
+++ src/services/products.service.ts
@@ -0,0 +1,15 @@
+import ProductsModel from '../models/products.model';
+import IProducts from '../interfaces/IProducts';
+import connection from '../models/connection';
+
+export default class ProductsService {
+  public model: ProductsModel;
+
+  constructor() {
+    this.model = new ProductsModel(connection);
+  }
+
+  public create(product: IProducts): Promise<IProducts> {
+    return this.model.create(product);
+  }
+}
